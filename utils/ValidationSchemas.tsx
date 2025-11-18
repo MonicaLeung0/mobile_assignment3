00a8d3f21e05d2f177c0238a24dcd57c8c0ef002
Edwin Olaez <edwinolaez@yahoo.com>
2025-11-17 19:25:40 -0700

    worked on app/index, constants - styles , created the components files to be finished by rest of the team

---
+++ utils/ValidationSchemas.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const validationSchemas = () => {
+  return (
+    <View>
+      <Text>validationSchemas</Text>
+    </View>
+  )
+}
+
+export default validationSchemas
+
+const styles = StyleSheet.create({})
